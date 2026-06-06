b40c34af01282d0dfe47bdd0be5336eed1e112b4
Andrey <bubband32@gmail.com>
2026-06-06 18:33:15 +0300

    add cron

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/go-co-op/gocron/v2"
 )
 
 const httpPort = ":3000"
@@ -20,9 +25,75 @@ func main() {
 		}
 	})
 
-	err := http.ListenAndServe(httpPort, r)
-
+	s, err := gocron.NewScheduler()
 	if err != nil {
 		panic(err)
 	}
+
+	jobs, err := initJobs(s)
+	if err != nil {
+		panic(err)
+	}
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		fmt.Println("Start server")
+		err := http.ListenAndServe(httpPort, r)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		fmt.Printf("Starting job: %v\n", jobs[0].ID())
+		s.Start()
+	}()
+
+	wg.Wait()
 }
+
+func initJobs(scheduler gocron.Scheduler) ([]gocron.Job, error) {
+
+	// add a job to the scheduler
+	j, err := scheduler.NewJob(
+		gocron.DurationJob(
+			10*time.Second,
+		),
+		gocron.NewTask(
+			func() {
+				fmt.Println("New task")
+			},
+		),
+	)
+	fmt.Println("check")
+	if err != nil {
+		return nil, err
+	}
+
+	return []gocron.Job{j}, nil
+
+}
+
+// func runCron() error {
+// 	// each job has a unique id
+// 	fmt.Println(j.ID())
+
+// 	// start the scheduler
+// 	s.Start()
+// 	// block until you are ready to shut down
+// 	select {
+// 	case <-time.After(time.Minute):
+// 	}
+
+// 	// when you're done, shut it down
+// 	err = s.Shutdown()
+// 	// or for context-aware teardown:
+// 	// err = s.ShutdownWithContext(ctx)
+// 	if err != nil {
+// 		// handle error
+// 	}
+// }
